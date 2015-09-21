b07e199d78e7bd0341d1f661030a70cb1efab8cb
Craige McWhirter <craige@mcwhirter.com.au>
2015-09-21 10:58:07 +1000

    Added HDMI key combo.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -7,6 +7,7 @@ import System.IO
 
 main = do
         xmproc <- spawnPipe "/usr/bin/xmobar /home/craige/.xmobarrc"
+        -- Launch xmobar as my task bar.
         xmonad $ defaultConfig
                 { focusFollowsMouse = False
                 , terminal = "terminology"
@@ -20,13 +21,18 @@ main = do
                 --, borderWidth = 1
                 } `additionalKeys`
                         -- Lock the screen
-                        [ ((0, 0x1008ff2d), spawn "xscreensaver-command -lock") -- XF86ScreenSaver
+                        [ ((0, 0x1008ff2d), spawn "xscreensaver-command -lock")
+                        -- XF86ScreenSaver
                         , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
                         , ((0, xK_Print), spawn "scrot")
                         -- Turn off the display port
                         , ((mod4Mask, xK_d), spawn "/usr/bin/xrandr --output DP1 --off")
                         -- Turn on the display port and set it as the primary display
                         , ((mod4Mask .|. shiftMask, xK_d), spawn "/usr/bin/xrandr --output DP1 --primary ; /usr/bin/xrandr --output LVDS1 --mode 1280x800; /usr/bin/xrandr --output DP1 --mode 2560x1440; /usr/bin/xrandr --output DP1 --left-of LVDS1")
+                        -- Turn off the HDMI port
+                        , ((mod4Mask, xK_h), spawn "/usr/bin/xrandr --output HDMI1 --off")
+                        -- Turn on the HDMI port and set it as the primary display
+                        , ((mod4Mask .|. shiftMask, xK_h), spawn "/usr/bin/xrandr --output HDMI1 --primary ; /usr/bin/xrandr --output LVDS1 --mode 1280x800; /usr/bin/xrandr --output HDMI1 --mode 1600x900; /usr/bin/xrandr --output HDMI1 --left-of LVDS1")
                         , ((mod4Mask, xK_v), spawn "/usr/bin/xrandr --output VGA1 --off")
                         , ((mod4Mask .|. shiftMask, xK_v), spawn " /usr/bin/xrandr --output VGA1 --primary ; /usr/bin/xrandr --output LVDS1 --mode 1280x800; /usr/bin/xrandr --output VGA1 --mode 1600x900; /usr/bin/xrandr --output VGA1 --left-of LVDS1")
                         , ((0 , 0x1008FF11), spawn "amixer set Master 2%-") -- XF86AudioLowerVolume
